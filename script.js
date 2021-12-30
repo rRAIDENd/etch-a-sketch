3e1d50b5ee1f456f627e9b2359afea544bebb074
Ridwan <61543323+rRAIDENd@users.noreply.github.com>
2021-12-30 14:02:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const grid = document.querySelector('#grid');
+
+
+function makeDiv(val){
+    for (let i=0; i < val; i++){
+        let row = document.createElement('div');
+        row.className = 'grid-row';
+        for (let c=0; c<val; c++){
+            let cell = document.createElement('div');
+            // cell.className = 'row-cell-' + val, 'cell';
+            cell.classList.add('cell')
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+
+makeDiv(9);
